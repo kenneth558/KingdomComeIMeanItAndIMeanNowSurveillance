2c2e482a747d2933e956fe4c04e87e5c411946cc
Kenneth L Anderson <17243233+kenneth558@users.noreply.github.com>
2021-08-15 02:10:33 -0500

    Now works for servers behind wifi router on intranet

--- etc/networkd-dispatcher/routable.d/getexternalipv4.sh
+++ etc/networkd-dispatcher/routable.d/getexternalipv4.sh
@@ -3,6 +3,7 @@
 touch /etc/networkd-dispatcher/routable.d/IFACEVAR
 EXTERNAL_INTERFACE=$(route|grep default|awk '{print $NF}')
 printf "${EXTERNAL_INTERFACE}" > /etc/networkd-dispatcher/routable.d/EXTERNAL_INTERFACE
+#next line -  Does it work?  Time will tell
 while [[ "$(ifconfig|grep '"${EXTERNAL_INTERFACE}": flags='|grep '<UP,')." == "." ]]; do
    sleep 1
 done
@@ -15,7 +16,14 @@ IFACElocal="$(</etc/networkd-dispatcher/routable.d/IFACEVAR)"
 #next line is from previous version of OS.  Does it work now?  Time will tell
 if [[ "${IFACElocal#IFACE=}" == "${EXTERNAL_INTERFACE}" ]]; then
    ADDRold="$(grep -m 1 -Eo "([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])" /etc/networkd-dispatcher/routable.d/iplog.txt)"
-   timeout 0.130 curl icanhazip.com > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl ipecho.net > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl ipinfo.io/ip > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl ifconfig.me > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl api.ipify.org > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl bot.whatismyipaddress.com > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null
+#the following are sorted by millisecond metric for their typical teim of execution, least to greatest.  Example of measuring statement:
+#date +%N > /tmp/starttime;curl icanhazip.com &> /dev/null;date +%N > /tmp/endtime;echo $(( $(cat /tmp/endtime) - $(cat /tmp/starttime) ))
+   timeout 0.130 curl icanhazip.com > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || \
+timeout 0.130 curl ipecho.net > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || \
+timeout 0.130 curl ipinfo.io/ip > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || \
+timeout 0.130 curl ifconfig.me > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || \
+timeout 0.130 curl api.ipify.org > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || \
+timeout 0.130 curl bot.whatismyipaddress.com > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null
    ADDRnew="$(grep -m 1 -Eo "([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])" /etc/networkd-dispatcher/routable.d/iplog.txt)"
    if [ "$ADDRold" != "$ADDRnew" ]; then
       mailingdirname="/etc/emails_awaiting/ipaddchngmail$(date +%Y%m%d%H%M%S)"
