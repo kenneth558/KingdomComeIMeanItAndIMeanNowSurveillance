c1618a1b7f835c1bafe9a75bb28efbce5f6df6d1
Kenneth L Anderson <17243233+kenneth558@users.noreply.github.com>
2021-08-14 20:26:37 -0500

    Now works for servers on intranet addresses

--- etc/networkd-dispatcher/routable.d/getexternalipv4.sh
+++ etc/networkd-dispatcher/routable.d/getexternalipv4.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#next line is nothing more than possibly useful for troubleshooting
+touch /etc/networkd-dispatcher/routable.d/IFACEVAR
 #next line make whatever the name of the external interface is
 while [[ "$(ifconfig|grep 'eno1: flags='|grep '<UP,')." == "." ]]; do
    sleep 1
@@ -6,13 +8,15 @@ done
 
 if [ ! -f /etc/networkd-dispatcher/routable.d/iplog.txt ]; then touch /etc/networkd-dispatcher/routable.d/iplog.txt; fi
 
-printf "${IFACE}" >> /etc/networkd-dispatcher/routable.d/$IFACE
+printf "${IFACE}," >> /etc/networkd-dispatcher/routable.d/IFACEs
 printf "$(env|grep 'IFACE=')" > /etc/networkd-dispatcher/routable.d/IFACEVAR
 IFACElocal="$(</etc/networkd-dispatcher/routable.d/IFACEVAR)"
-if [[ "$IFACElocal" == "IFACE=--all" ]]; then
-   ADDRold="$(cat /etc/networkd-dispatcher/routable.d/iplog.txt)"
+#IFACE=eno1
+#next line is from previous version of OS.  Does it work now?  Time will tell
+if [[ "${IFACElocal#IFACE=}" == "eno1" ]]; then
+   ADDRold="$(grep -Eo "([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])" /etc/networkd-dispatcher/routable.d/iplog.txt)"
    timeout 0.130 curl icanhazip.com > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl ipecho.net > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl ipinfo.io/ip > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl ifconfig.me > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl api.ipify.org > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null || timeout 0.130 curl bot.whatismyipaddress.com > /etc/networkd-dispatcher/routable.d/iplog.txt 2> /dev/null
-   ADDRnew="$(cat /etc/networkd-dispatcher/routable.d/iplog.txt)"
+   ADDRnew="$(grep -Eo "([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])" /etc/networkd-dispatcher/routable.d/iplog.txt)"
    if [ "$ADDRold" != "$ADDRnew" ]; then
       mailingdirname="/etc/emails_awaiting/ipaddchngmail$(date +%Y%m%d%H%M%S)"
       mkdir "$mailingdirname" 2>/dev/null
